50b0d94b75388a4bb69a3eb164e9cc4fcc6b89a0
rHermes <teodor@spaeren.no>
2020-12-03 08:32:27 +0100

    Year 2020 Day 3
    Easy, made slightly harder by the fact that I didn't want to store the
    positions in an array. This is kind of moot in my case as the program
    reads in the entire input at once, but my algorithm would work for an
    input stream aswell.

--- years/y2020/day03.go
+++ years/y2020/day03.go
@@ -1,13 +1,49 @@
 package y2020
 
 import (
-	"errors"
+	"bytes"
+	"strconv"
 )
 
 func Day03Part01(input []byte) (string, error) {
-	return "", errors.New("Not implemented")
+	ans := uint64(0)
+	for i, line := range bytes.Split(input, []byte{'\n'}) {
+		if len(line) > 0 && line[(i*3)%len(line)] == '#' {
+			ans++
+		}
+	}
+	return strconv.FormatUint(ans, 10), nil
 }
 
 func Day03Part02(input []byte) (string, error) {
-	return "", errors.New("Not implemented")
+	type Slope struct {
+		dx int
+		dy int
+	}
+	slopes := map[Slope]uint64{
+		Slope{1, 1}: 0,
+		Slope{3, 1}: 0,
+		Slope{5, 1}: 0,
+		Slope{7, 1}: 0,
+		Slope{1, 2}: 0,
+	}
+
+	for i, line := range bytes.Split(input, []byte{'\n'}) {
+		if len(line) == 0 {
+			continue
+		}
+
+		for k, _ := range slopes {
+			y := i / k.dy
+			if i%k.dy == 0 && line[(y*k.dx)%len(line)] == '#' {
+				slopes[k]++
+			}
+		}
+	}
+
+	ans := uint64(1)
+	for _, v := range slopes {
+		ans *= v
+	}
+	return strconv.FormatUint(ans, 10), nil
 }
