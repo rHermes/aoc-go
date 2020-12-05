97d27d5d1705fdf3a9e67dbade9d61b34099c873
rHermes <teodor_spaeren@riseup.net>
2020-12-05 13:58:39 +0100

    2020 Day 4
    Ugly solution in go, but atleast a solution.

--- main.go
+++ main.go
@@ -15,8 +15,18 @@ func main() {
 	app := &cli.App{
 		Name:  "aoc-go",
 		Usage: "Advent of code in go",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "session",
+				Usage:       "Used to interact with the aoc website",
+				DefaultText: "<HIDDEN>",
+				EnvVars:     []string{"AOC_SESSION"},
+			},
+		},
+
 		Commands: []*cli.Command{
 			RunCommand,
+			LeaderboardCommand,
 		},
 	}
 
@@ -26,65 +36,21 @@ func main() {
 	}
 }
 
+var LeaderboardCommand = &cli.Command{
+	Name:  "board",
+	Usage: "print out local leaderboard",
+	Action: func(c *cli.Context) error {
+		fmt.Println(c.String("session"))
+		return errors.New("Not implemented yet")
+	},
+}
+
 type TaskFunc func([]byte) (string, error)
 
 type Task struct {
 	Year int
 	Day  int
 	Part int
-}
-
-var ydp = map[Task]TaskFunc{
-	Task{2020, 1, 1}:  y2020.Day01Part01,
-	Task{2020, 1, 2}:  y2020.Day01Part02,
-	Task{2020, 2, 1}:  y2020.Day02Part01,
-	Task{2020, 2, 2}:  y2020.Day02Part02,
-	Task{2020, 3, 1}:  y2020.Day03Part01,
-	Task{2020, 3, 2}:  y2020.Day03Part02,
-	Task{2020, 4, 1}:  y2020.Day04Part01,
-	Task{2020, 4, 2}:  y2020.Day04Part02,
-	Task{2020, 5, 1}:  y2020.Day05Part01,
-	Task{2020, 5, 2}:  y2020.Day05Part02,
-	Task{2020, 6, 1}:  y2020.Day06Part01,
-	Task{2020, 6, 2}:  y2020.Day06Part02,
-	Task{2020, 7, 1}:  y2020.Day07Part01,
-	Task{2020, 7, 2}:  y2020.Day07Part02,
-	Task{2020, 8, 1}:  y2020.Day08Part01,
-	Task{2020, 8, 2}:  y2020.Day08Part02,
-	Task{2020, 9, 1}:  y2020.Day09Part01,
-	Task{2020, 9, 2}:  y2020.Day09Part02,
-	Task{2020, 10, 1}: y2020.Day10Part01,
-	Task{2020, 10, 2}: y2020.Day10Part02,
-	Task{2020, 11, 1}: y2020.Day11Part01,
-	Task{2020, 11, 2}: y2020.Day11Part02,
-	Task{2020, 12, 1}: y2020.Day12Part01,
-	Task{2020, 12, 2}: y2020.Day12Part02,
-	Task{2020, 13, 1}: y2020.Day13Part01,
-	Task{2020, 13, 2}: y2020.Day13Part02,
-	Task{2020, 14, 1}: y2020.Day14Part01,
-	Task{2020, 14, 2}: y2020.Day14Part02,
-	Task{2020, 15, 1}: y2020.Day15Part01,
-	Task{2020, 15, 2}: y2020.Day15Part02,
-	Task{2020, 16, 1}: y2020.Day16Part01,
-	Task{2020, 16, 2}: y2020.Day16Part02,
-	Task{2020, 17, 1}: y2020.Day17Part01,
-	Task{2020, 17, 2}: y2020.Day17Part02,
-	Task{2020, 18, 1}: y2020.Day18Part01,
-	Task{2020, 18, 2}: y2020.Day18Part02,
-	Task{2020, 19, 1}: y2020.Day19Part01,
-	Task{2020, 19, 2}: y2020.Day19Part02,
-	Task{2020, 20, 1}: y2020.Day20Part01,
-	Task{2020, 20, 2}: y2020.Day20Part02,
-	Task{2020, 21, 1}: y2020.Day21Part01,
-	Task{2020, 21, 2}: y2020.Day21Part02,
-	Task{2020, 22, 1}: y2020.Day22Part01,
-	Task{2020, 22, 2}: y2020.Day22Part02,
-	Task{2020, 23, 1}: y2020.Day23Part01,
-	Task{2020, 23, 2}: y2020.Day23Part02,
-	Task{2020, 24, 1}: y2020.Day24Part01,
-	Task{2020, 24, 2}: y2020.Day24Part02,
-	Task{2020, 25, 1}: y2020.Day25Part01,
-	Task{2020, 25, 2}: y2020.Day25Part02,
 }
 
 var RunCommand = &cli.Command{
@@ -139,4 +105,57 @@ var RunCommand = &cli.Command{
 
 		return nil
 	},
+}
+
+var ydp = map[Task]TaskFunc{
+	Task{2020, 1, 1}:  y2020.Day01Part01,
+	Task{2020, 1, 2}:  y2020.Day01Part02,
+	Task{2020, 2, 1}:  y2020.Day02Part01,
+	Task{2020, 2, 2}:  y2020.Day02Part02,
+	Task{2020, 3, 1}:  y2020.Day03Part01,
+	Task{2020, 3, 2}:  y2020.Day03Part02,
+	Task{2020, 4, 1}:  y2020.Day04Part01,
+	Task{2020, 4, 2}:  y2020.Day04Part02,
+	Task{2020, 5, 1}:  y2020.Day05Part01,
+	Task{2020, 5, 2}:  y2020.Day05Part02,
+	Task{2020, 6, 1}:  y2020.Day06Part01,
+	Task{2020, 6, 2}:  y2020.Day06Part02,
+	Task{2020, 7, 1}:  y2020.Day07Part01,
+	Task{2020, 7, 2}:  y2020.Day07Part02,
+	Task{2020, 8, 1}:  y2020.Day08Part01,
+	Task{2020, 8, 2}:  y2020.Day08Part02,
+	Task{2020, 9, 1}:  y2020.Day09Part01,
+	Task{2020, 9, 2}:  y2020.Day09Part02,
+	Task{2020, 10, 1}: y2020.Day10Part01,
+	Task{2020, 10, 2}: y2020.Day10Part02,
+	Task{2020, 11, 1}: y2020.Day11Part01,
+	Task{2020, 11, 2}: y2020.Day11Part02,
+	Task{2020, 12, 1}: y2020.Day12Part01,
+	Task{2020, 12, 2}: y2020.Day12Part02,
+	Task{2020, 13, 1}: y2020.Day13Part01,
+	Task{2020, 13, 2}: y2020.Day13Part02,
+	Task{2020, 14, 1}: y2020.Day14Part01,
+	Task{2020, 14, 2}: y2020.Day14Part02,
+	Task{2020, 15, 1}: y2020.Day15Part01,
+	Task{2020, 15, 2}: y2020.Day15Part02,
+	Task{2020, 16, 1}: y2020.Day16Part01,
+	Task{2020, 16, 2}: y2020.Day16Part02,
+	Task{2020, 17, 1}: y2020.Day17Part01,
+	Task{2020, 17, 2}: y2020.Day17Part02,
+	Task{2020, 18, 1}: y2020.Day18Part01,
+	Task{2020, 18, 2}: y2020.Day18Part02,
+	Task{2020, 19, 1}: y2020.Day19Part01,
+	Task{2020, 19, 2}: y2020.Day19Part02,
+	Task{2020, 20, 1}: y2020.Day20Part01,
+	Task{2020, 20, 2}: y2020.Day20Part02,
+	Task{2020, 21, 1}: y2020.Day21Part01,
+	Task{2020, 21, 2}: y2020.Day21Part02,
+	Task{2020, 22, 1}: y2020.Day22Part01,
+	Task{2020, 22, 2}: y2020.Day22Part02,
+	Task{2020, 23, 1}: y2020.Day23Part01,
+	Task{2020, 23, 2}: y2020.Day23Part02,
+	Task{2020, 24, 1}: y2020.Day24Part01,
+	Task{2020, 24, 2}: y2020.Day24Part02,
+	Task{2020, 25, 1}: y2020.Day25Part01,
+	Task{2020, 25, 2}: y2020.Day25Part02,
 }
--- years/y2020/day04.go
+++ years/y2020/day04.go
@@ -1,13 +1,119 @@
 package y2020
 
 import (
-	"errors"
+	"bytes"
+	"strconv"
 )
 
+func d4ValidateDate(a []byte, from, to uint64, size int) uint8 {
+	if size > 0 {
+		if len(a) != size {
+			return 0
+		}
+	}
+	i, err := strconv.ParseUint(string(a), 10, 64)
+	if err == nil && (from <= i) && (i <= to) {
+		return 1
+	} else {
+		return 0
+	}
+}
+
+func d4ValidateHeight(a []byte) uint8 {
+	i, err := strconv.ParseUint(string(a[:len(a)-2]), 10, 64)
+	if err != nil {
+		return 0
+	}
+
+	switch string(a[len(a)-2:]) {
+	case "cm":
+		if 150 <= i && i <= 193 {
+			return 1
+		}
+	case "in":
+		if 59 <= i && i <= 76 {
+			return 1
+		}
+	}
+
+	return 0
+}
+
+func d4ValidatEyeColor(a []byte) uint8 {
+	switch string(a) {
+	case "amb", "blu", "brn", "gry", "grn", "hzl", "oth":
+		return 1
+	default:
+		return 0
+	}
+}
+
+func d4ValidateHairColor(a []byte) uint8 {
+	if len(a) != 7 || a[0] != '#' || len(bytes.Trim(a[1:], "abcdef0123456789")) != 0 {
+		return 0
+	} else {
+		return 1
+	}
+}
+
+// Return the hash of the key
+func d4hash(p []byte, validate bool) uint8 {
+	sw := uint8(0)
+	if validate {
+		sw = 1
+	}
+	switch string(p[:4]) {
+	case "byr:":
+		return 1 << ((1-sw)*1 + sw*d4ValidateDate(p[4:], 1920, 2002, 4)*1)
+	case "iyr:":
+		return 1 << ((1-sw)*2 + sw*d4ValidateDate(p[4:], 2010, 2020, 4)*2)
+	case "eyr:":
+		return 1 << ((1-sw)*3 + sw*d4ValidateDate(p[4:], 2020, 2030, 4)*3)
+	case "hgt:":
+		return 1 << ((1-sw)*4 + sw*d4ValidateHeight(p[4:])*4)
+	case "hcl:":
+		return 1 << ((1-sw)*5 + sw*d4ValidateHairColor(p[4:])*5)
+	case "ecl:":
+		return 1 << ((1-sw)*6 + sw*d4ValidatEyeColor(p[4:])*6)
+	case "pid:":
+		return 1 << ((1-sw)*7 + sw*d4ValidateDate(p[4:], 0, 999999999, 9)*7)
+	case "cid:":
+		return 0
+	default:
+		return 1
+	}
+}
+
 func Day04Part01(input []byte) (string, error) {
-	return "", errors.New("Not implemented")
+	ans := uint64(0)
+	for _, a := range bytes.Split(bytes.TrimSpace(input), []byte{'\n', '\n'}) {
+		a = bytes.ReplaceAll(a, []byte{'\n'}, []byte{' '})
+
+		// Use bit flags
+		f := uint8(0)
+		for _, p := range bytes.Split(a, []byte{' '}) {
+			f |= d4hash(p, false)
+		}
+		if f == 0b11111110 {
+			ans++
+		}
+	}
+	return strconv.FormatUint(ans, 10), nil
 }
 
 func Day04Part02(input []byte) (string, error) {
-	return "", errors.New("Not implemented")
+	ans := uint64(0)
+	for _, a := range bytes.Split(bytes.TrimSpace(input), []byte{'\n', '\n'}) {
+		a = bytes.ReplaceAll(a, []byte{'\n'}, []byte{' '})
+
+		// Use bit flags
+		f := uint8(0)
+		for _, p := range bytes.Split(a, []byte{' '}) {
+			f |= d4hash(p, true)
+		}
+		if f == 0b11111110 {
+			ans++
+		}
+	}
+	return strconv.FormatUint(ans, 10), nil
 }
