6c3182e706b0db902efa8d95819922505512d85d
rHermes <teodor@spaeren.no>
2020-12-06 08:03:10 +0100

    2020 Day 6
    This was rather elegant in go! I like that I could count the letters and
    use the newline to see how many times it appeared to calculate the
    number of lines.

--- years/y2020/day06.go
+++ years/y2020/day06.go
@@ -1,13 +1,38 @@
 package y2020
 
 import (
-	"errors"
+	"bytes"
+	"strconv"
 )
 
 func Day06Part01(input []byte) (string, error) {
-	return "", errors.New("Not implemented")
+	var ans int
+	for _, group := range bytes.Split(input, []byte{'\n', '\n'}) {
+		s := map[byte]struct{}{}
+		for _, c := range group {
+			s[c] = struct{}{}
+		}
+		delete(s, '\n')
+		ans += len(s)
+	}
+
+	return strconv.Itoa(ans), nil
 }
 
 func Day06Part02(input []byte) (string, error) {
-	return "", errors.New("Not implemented")
+	var ans int
+	for _, group := range bytes.Split(input, []byte{'\n', '\n'}) {
+		s := map[byte]uint64{}
+		for _, c := range group {
+			s[c]++
+		}
+		lines := s['\n'] + 1
+		for _, v := range s {
+			if v == lines {
+				ans++
+			}
+		}
+	}
+
+	return strconv.Itoa(ans), nil
 }
